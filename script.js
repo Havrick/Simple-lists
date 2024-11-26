838194df7a890b00977607fd40f347c585a06a3f
Havrick <asrahdr@gmail.com>
2024-11-26 20:18:51 +0100

    In the addItemToView before i used the add to theList.innerHTML but so when you added one div and you have something write on it the next time you added another one the content was lost. Instead i using now the queryselector and then use the insertAdjancentHtml before the end. Added more functionality to addOrUpdateItem but more work needs to be done here.

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 const addItem = document.getElementById("add-item");
-const theList = document.getElementById("the-list");
 
 const list = [];
+let currentItem = {};
 
-const addItemToView = ()=>{
-    theList.innerHTML += `
-    <div>
-        <input type="checkbox">
-        <input txpe="text" onchange="addOrUpdateItem()">
+// Adds div with the id to the page, the div contains checkbox, a textbox and a delete button. 
+const addItemsToView = ()=>{
+    const theList = document.querySelector("#the-list");
+    const HTMLString = `
+    <div id="${Date.now()}">
+        <input type="checkbox" onchange="addOrUpdateItem(this)">
+        <input type="text" onchange="addOrUpdateItem(this)">
         <button type="button">x</button>
     </div>
     `;
+    theList.insertAdjacentHTML("beforeend", HTMLString);
 };
 
-const addOrUpdateItem = ()=>{
-    console.log("hello");
+// Add or update the item to the list.
+const addOrUpdateItem = (elementEl)=>{
+    const id = elementEl.parentElement.id;
+    currentItem = list.find((item) => item.id === id);
+    if(!currentItem){
+        const newItem = {
+            id: id,
+            isChecked: elementEl.type === "checkbox" ? true : false,
+            title: elementEl.type === "text" ? elementEl.value : "" , 
+        };
+        list.push(newItem);
+    }
+    currentItem = {};
+    console.log(list);
 };
-addItem.addEventListener("click", addItemToView);
+addItem.addEventListener("click", addItemsToView);
